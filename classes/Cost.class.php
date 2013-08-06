dd28b154a9e9e51a8f6e6dfa0a2e19e4b07e2358
chriscornelis <chriscornelis_10@hotmail.com>
2013-08-06 14:56:25 +0200

    Cost.class.php aangemaakt
    Klasse om de uitgaven te beheren

---
+++ classes/Cost.class.php
@@ -0,0 +1,84 @@
+<?php
+class Cost
+{
+	private $m_sCostID;
+	private $m_sListID;
+	private $m_sTypeID;
+	private $m_sPrice;
+	private $m_sDate;
+	private $m_sInfo;
+	private $m_sStartKm;
+	private $m_sEndKm;
+	
+	public function _set($p_sProperty, $p_vValue)
+	{
+		switch($p_sProperty)
+		{
+			case "CostID":
+				$this->m_sCostID = $p_vValue;
+				break;
+			case "ListID":
+				$this->m_sListID = $p_vValue;
+				break;
+			case "TypeID":
+				$this->m_sTypeID = $p_vValue;
+				break;
+			case "Price":
+				$this->m_sPrice = $p_vValue;
+				break;
+			case "Date":
+				$this->m_sDate = $p_vValue;
+				break;
+			case "Info":
+				$this->m_sInfo = $p_vValue;
+				break;
+			case "StartKm":
+				$this->m_sStartKm = $p_vValue;
+				break;
+			case "EndKm":
+				$this->m_sEndKm = $p_vValue;
+				break;
+		}
+	}
+	public function _get($p_sProperty)
+	{
+		$lResult = null;
+		switch($p_sProperty)
+		{
+			case "CostID":
+				$lResult = $this->m_sCostID;
+				break;
+			case "ListID":
+				$lResult = $this->m_sListID;
+				break;
+			case "TypeID":
+				$lResult = $this->m_sTypeID;
+				break;
+			case "Price":
+				$lResult = $this->m_sPrice;
+				break;
+			case "Date":
+				$lResult = $this->m_sDate;
+				break;
+			case "Info":
+				$lResult = $this->m_sInfo;
+				break;
+			case "StartKm":
+				$lResult = $this->m_sStartKm;
+				break;
+			case "EndKm":
+				$lResult = $this->m_sEndKm;
+				break;
+		}
+		return $lResult;
+	}
+	public function saveCost()
+	{
+		
+	}
+	public function getAllCostOfList()
+	{
+	
+	}
+}
+?>
