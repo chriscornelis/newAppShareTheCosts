0f5474cae55a96d2d242e6af10d427838428dba2
chriscornelis <chriscornelis_10@hotmail.com>
2013-08-06 14:54:38 +0200

    footer knop
    'lijsten' veranderd in 'mijn lijsten'

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div data-role="footer" data-id="foo1" data-position="fixed">
 				<div data-role="navbar">
 				    <ul>
-				        <li><a href="overzichtlijsten.php" data-icon="bars" class="ui-btn-active">LIJSTEN</a></li>
+				        <li><a href="overzichtlijsten.php" data-icon="bars" class="ui-btn-active">MIJN LIJSTEN</a></li>
 				        <li><a href="lijstmaken.php" data-icon="edit">MAAK LIJST</a></li>
 				        <li><a href="zoek.php" data-icon="search">ZOEK</a></li>
 				    </ul>
